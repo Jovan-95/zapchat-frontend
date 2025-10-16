2ab48bcb5b4c9e7fe97f865743acb8c4aaa40181
jovan <vuksanovicjovan@hotmail.com>
2025-10-16 12:28:43 +0200

    env link fix

--- src/pusherClient.ts
+++ src/pusherClient.ts
@@ -3,17 +3,18 @@ import Pusher from "pusher-js";
 
 let pusher: Pusher | null = null;
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const initPusher = (token: string) => {
   if (pusher) {
     pusher.disconnect(); // za svaki slučaj ako već postoji stara konekcija
   }
-
+  console.log("Token", token);
   pusher = new Pusher("c62c108160e4b23c8f71", {
     cluster: "eu",
     forceTLS: true,
     // authEndpoint: "http://127.0.0.1:8000/broadcasting/auth",
-    authEndpoint:
-      "https://zap-chat-backend-production-03b2.up.railway.app/broadcasting/auth",
+    authEndpoint: `${API_URL}/broadcasting/auth`,
 
     auth: {
       headers: {
--- src/services/authServices.tsx
+++ src/services/authServices.tsx
@@ -2,6 +2,7 @@ import { LoginFormUser, RegisterFormUser, ResetUserObj } from "../types/type";
 
 // const API_URL = "http://127.0.0.1:8000/api";
 // const API_URL = "https://zap-chat-backend-production-03b2.up.railway.app/api";
+
 const API_URL = import.meta.env.VITE_API_URL;
 console.log("API_URL:", API_URL);
 
