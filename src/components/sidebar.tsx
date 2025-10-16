7e41e8744585269a7b57bcf5f362f84d1422f4f1
jovan <vuksanovicjovan@hotmail.com>
2025-10-16 20:56:35 +0200

    build fix

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../redux/store";
+import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logoutUser } from "../services/authServices";
@@ -8,78 +7,78 @@ import { logoutUserFromReduxAndLS } from "../redux/slice";
 // import { pusher } from "../pusherClient";
 
 function Sidebar() {
-    const dispatch = useDispatch();
-    const queryClient = useQueryClient();
+  const dispatch = useDispatch();
+  const queryClient = useQueryClient();
 
-    // Logged user from Redux
-    // const loggedUser = useSelector(
-    //     (state: RootState) => state.auth.loggedInUser
-    // );
+  // Logged user from Redux
+  // const loggedUser = useSelector(
+  //     (state: RootState) => state.auth.loggedInUser
+  // );
 
-    // HTTP POST
-    const logoutUserMutation = useMutation({
-        mutationFn: logoutUser,
-        onSuccess: () => {
-            // console.log("Temporary use because of build", data);
+  // HTTP POST
+  const logoutUserMutation = useMutation({
+    mutationFn: logoutUser,
+    onSuccess: () => {
+      // console.log("Temporary use because of build", data);
 
-            // Clear localStorage
-            localStorage.removeItem("loggedInUser");
-            localStorage.removeItem("auth_token");
+      // Clear localStorage
+      localStorage.removeItem("loggedInUser");
+      localStorage.removeItem("auth_token");
 
-            // Redux
-            dispatch(logoutUserFromReduxAndLS());
+      // Redux
+      dispatch(logoutUserFromReduxAndLS());
 
-            // Navigation
+      // Navigation
 
-            window.location.href = "/login"; // navigate and refresh home
-        },
-        onError: (err) => {
-            console.log("Temporary use because of build", err);
+      window.location.href = "/login"; // navigate and refresh home
+    },
+    onError: (err) => {
+      console.log("Temporary use because of build", err);
 
-            alert("Logout failed!");
-        },
-    });
+      alert("Logout failed!");
+    },
+  });
 
-    function handleLogout() {
-        logoutUserMutation.mutate();
-        queryClient.clear();
-    }
-    return (
-        <div className="sidebar">
-            <div className="logo-wrapper">
-                <img src="/icons/zc-logo.png" alt="logo" />
-            </div>
-            <div className="nav">
-                <NavLink to={"/"}>
-                    {" "}
-                    <div className="nav-item">
-                        <img src="/icons/chat-icon.png" alt="chat icon" />
-                        {/* <span>Chat</span> */}
-                    </div>
-                </NavLink>
-                <NavLink to={"/settings"}>
-                    {" "}
-                    <div className="nav-item">
-                        <img src="/icons/settings-icon.png" alt="logo" />
-                        {/* <span>Settings</span> */}
-                    </div>
-                </NavLink>
-            </div>
-            <div className="profile">
-                {/* <img
+  function handleLogout() {
+    logoutUserMutation.mutate();
+    queryClient.clear();
+  }
+  return (
+    <div className="sidebar">
+      <div className="logo-wrapper">
+        <img src="/icons/zc-logo.png" alt="logo" />
+      </div>
+      <div className="nav">
+        <NavLink to={"/"}>
+          {" "}
+          <div className="nav-item">
+            <img src="/icons/chat-icon.png" alt="chat icon" />
+            {/* <span>Chat</span> */}
+          </div>
+        </NavLink>
+        <NavLink to={"/settings"}>
+          {" "}
+          <div className="nav-item">
+            <img src="/icons/settings-icon.png" alt="logo" />
+            {/* <span>Settings</span> */}
+          </div>
+        </NavLink>
+      </div>
+      <div className="profile">
+        {/* <img
                     src={`http://localhost:8000/${loggedUser?.image_path}`}
                     alt="Profile img"
                 /> */}
-                <div>
-                    <img
-                        onClick={handleLogout}
-                        src="/icons/logout-icon.png"
-                        alt="logout icon"
-                    />
-                </div>
-            </div>
+        <div>
+          <img
+            onClick={handleLogout}
+            src="/icons/logout-icon.png"
+            alt="logout icon"
+          />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
--- src/types/interfaces.tsx
+++ src/types/interfaces.tsx
@@ -1,84 +1,80 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { ReactNode } from "react";
-import { ChatMessage, LoggedUser, Messages, User } from "./type";
+import { ChatMessage, LoggedUser, User } from "./type";
 
 // Tipiziranje za children (PrivateRoute)
 export interface PrivateRouteProps {
-    children: ReactNode;
-    requiredRole?: "admin" | "user"; // ili samo 'admin' ako ti ne treba za 'user'
+  children: ReactNode;
+  requiredRole?: "admin" | "user"; // ili samo 'admin' ako ti ne treba za 'user'
 }
 
 export interface PublicRouteProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export interface SendMessagePayload {
-    to_id: number; // ID korisnika kojem šalješ
-    message: string;
+  to_id: number; // ID korisnika kojem šalješ
+  message: string;
 }
 
 // User List props type
 export interface UsersListProps {
-    loggedUser: LoggedUser | null;
-    setTargetUser: React.Dispatch<React.SetStateAction<User | null>>;
-    setShowConversation: React.Dispatch<React.SetStateAction<boolean>>;
-    onlineUsers: OnlineUser[];
+  loggedUser: LoggedUser | null;
+  setTargetUser: React.Dispatch<React.SetStateAction<User | null>>;
+  setShowConversation: React.Dispatch<React.SetStateAction<boolean>>;
+  onlineUsers: OnlineUser[];
 }
 
 // Chat input props type
 export interface ChatInputProps {
-    loggedUser: LoggedUser | null;
-    targetUser: User | null;
-    message: string;
-    setMessage: React.Dispatch<React.SetStateAction<string>>;
-    setConversationMessages: React.Dispatch<
-        React.SetStateAction<ChatMessage[]>
-    >;
+  loggedUser: LoggedUser | null;
+  targetUser: User | null;
+  message: string;
+  setMessage: React.Dispatch<React.SetStateAction<string>>;
+  setConversationMessages: React.Dispatch<React.SetStateAction<ChatMessage[]>>;
 }
 
 export interface ConversationProps {
-    conversationMessages: ChatMessage[];
-    targetUser: User | null;
-    loggedUser: LoggedUser | null;
-    setShowConversation: React.Dispatch<React.SetStateAction<boolean>>;
-    typingUsers: Record<number, boolean>;
+  conversationMessages: ChatMessage[];
+  targetUser: User | null;
+  loggedUser: LoggedUser | null;
+  setShowConversation: React.Dispatch<React.SetStateAction<boolean>>;
+  typingUsers: Record<number, boolean>;
 }
 
 // Online user obj
 export interface OnlineUser {
-    created_at: string;
-    email: string;
-    email_verified_at: null;
-    id: number;
-    image_path: string;
-    is_admin: number;
-    last_name: string;
-    name: string;
-    updated_at: string;
-    username: string;
+  created_at: string;
+  email: string;
+  email_verified_at: null;
+  id: number;
+  image_path: string;
+  is_admin: number;
+  last_name: string;
+  name: string;
+  updated_at: string;
+  username: string;
 }
 
 // Pusher live members
 export interface Members {
-    count: number;
-    me: { id: string; info: OnlineUser };
-    members: OnlineUser;
-    myID: string;
+  count: number;
+  me: { id: string; info: OnlineUser };
+  members: OnlineUser;
+  myID: string;
 }
 
 // Home props
 export interface HomeProps {
-    onlineUsers: OnlineUser[];
-    targetUser: User | null;
-    setTargetUser: React.Dispatch<React.SetStateAction<User | null>>;
-    conversationMessages: ChatMessage[];
-    setConversationMessages: React.Dispatch<
-        React.SetStateAction<ChatMessage[]>
-    >;
-    typingUsers: Record<number, boolean>;
+  onlineUsers: OnlineUser[];
+  targetUser: User | null;
+  setTargetUser: React.Dispatch<React.SetStateAction<User | null>>;
+  conversationMessages: ChatMessage[];
+  setConversationMessages: React.Dispatch<React.SetStateAction<ChatMessage[]>>;
+  typingUsers: Record<number, boolean>;
 }
 
 // User live typing event
 export interface UserTypingEvent {
-    fromId: number;
+  fromId: number;
 }
