39682b3506769b6f6f1babb99187647c6dca0488
jovan <vuksanovicjovan@hotmail.com>
2025-10-19 14:02:22 +0200

    fix

--- src/components/mobileHeader.tsx
+++ src/components/mobileHeader.tsx
@@ -8,92 +8,86 @@ import { useDispatch } from "react-redux";
 import { showErrorToast } from "./toast";
 
 function MobileHeader() {
-    const [mobModal, setMobModal] = useState(false);
-    const queryClient = useQueryClient();
-    const dispatch = useDispatch();
+  const [mobModal, setMobModal] = useState(false);
+  const queryClient = useQueryClient();
+  const dispatch = useDispatch();
 
-    // HTTP POST
-    const logoutUserMutation = useMutation({
-        mutationFn: logoutUser,
-        onSuccess: () => {
-            // Clear localStorage
-            localStorage.removeItem("loggedInUser");
-            localStorage.removeItem("auth_token");
+  // HTTP POST
+  const logoutUserMutation = useMutation({
+    mutationFn: logoutUser,
+    onSuccess: () => {
+      // Clear localStorage
+      localStorage.removeItem("loggedInUser");
+      localStorage.removeItem("auth_token");
 
-            // Redux
-            dispatch(logoutUserFromReduxAndLS());
+      // Redux
+      dispatch(logoutUserFromReduxAndLS());
 
-            // Navigation
-            window.location.href = "/login"; // navigate and refresh home
-        },
-        onError: (err) => {
-            console.log("Temporary use because of build", err);
+      // Navigation
+      window.location.href = "/login"; // navigate and refresh home
+    },
+    onError: (err) => {
+      console.log("Temporary use because of build", err);
 
-            showErrorToast("Logout failed!");
-        },
-    });
+      showErrorToast("Logout failed!");
+    },
+  });
 
-    function handleLogout() {
-        logoutUserMutation.mutate();
-        queryClient.clear();
-    }
-    return (
-        <>
-            <div className="mobille-header-wrapper">
-                <div className="menu-icon-wrapper">
-                    <img
-                        onClick={() => setMobModal((prev) => !prev)}
-                        src="icons/hamburger-menu-icon.png"
-                        alt="mobile-menu-icon"
-                    />
-                </div>
+  function handleLogout() {
+    logoutUserMutation.mutate();
+    queryClient.clear();
+  }
+  return (
+    <>
+      <div className="mobille-header-wrapper">
+        <div className="menu-icon-wrapper">
+          <img
+            onClick={() => setMobModal((prev) => !prev)}
+            src="icons/hamburger-menu-icon.png"
+            alt="mobile-menu-icon"
+          />
+        </div>
+      </div>
+      {mobModal ? (
+        <MobileModal>
+          <div>
+            <div className="close-icon-wrapper">
+              <img
+                onClick={() => setMobModal(false)}
+                src="icons/close-icon.png"
+                alt="close-icon"
+              />
             </div>
-            {mobModal ? (
-                <MobileModal>
-                    <div>
-                        <div className="close-icon-wrapper">
-                            <img
-                                onClick={() => setMobModal(false)}
-                                src="icons/close-icon.png"
-                                alt="close-icon"
-                            />
-                        </div>
-                        <div className="navigation">
-                            {" "}
-                            <NavLink
-                                to={"/"}
-                                onClick={() => setMobModal(false)}
-                            >
-                                {" "}
-                                <div className="nav-item">
-                                    <span>Chat</span>
-                                </div>
-                            </NavLink>
-                            <NavLink
-                                to={"/settings"}
-                                onClick={() => setMobModal(false)}
-                            >
-                                {" "}
-                                <div className="nav-item">
-                                    <span>Settings</span>
-                                </div>
-                            </NavLink>
-                            <div className="nav-item">
-                                <span onClick={handleLogout}>Logout</span>
-                                {/* <img
+            <div className="navigation">
+              {" "}
+              <NavLink to={"/"} onClick={() => setMobModal(false)}>
+                {" "}
+                <div className="nav-item">
+                  <span>Chat</span>
+                </div>
+              </NavLink>
+              <NavLink to={"/settings"} onClick={() => setMobModal(false)}>
+                {" "}
+                <div className="nav-item">
+                  <span>Settings</span>
+                </div>
+              </NavLink>
+              <div className="nav-item">
+                <span onClick={handleLogout}>Logout</span>
+                {/* <img
                                     onClick={handleLogout}
                                     src="/icons/logout-icon.png"
                                     alt="logout icon"
                                 /> */}
-                            </div>
-                        </div>
-                    </div>
-                </MobileModal>
-            ) : (
-                ""
-            )}
-        </>
-    );
+              </div>
+            </div>
+          </div>
+        </MobileModal>
+      ) : (
+        ""
+      )}
+    </>
+  );
 }
 
 export default MobileHeader;
